ad77fde595c1161b4d57b984b45e1b145d50ecad
olegdoronenko <61374593+olegdoronenko@users.noreply.github.com>
2023-06-16 08:15:20 +0300

    ауф - инМемори админ и юзер работают не работает логаут

--- src/main/java/ru/kata/spring/boot_security/demo/controllers/AdminController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/AdminController.java
@@ -1,14 +1,15 @@
 package ru.kata.spring.boot_security.demo.controllers;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ru.kata.spring.boot_security.demo.configs.SuccessUserHandler;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
-
 
     @GetMapping()
     public  String admin() {
