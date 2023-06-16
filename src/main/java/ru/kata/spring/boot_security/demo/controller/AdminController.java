cc6aa4e44fe64ba3110ced5bb02d8cc32f797ca0
olegdoronenko <61374593+olegdoronenko@users.noreply.github.com>
2023-06-16 08:45:10 +0300

    ауф - инМемори админ и юзер работают не работает логаут

--- src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
@@ -1,20 +1,16 @@
-package ru.kata.spring.boot_security.demo.controllers;
+package ru.kata.spring.boot_security.demo.controller;
 
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/")
-public class WelcomeController {
+@RequestMapping("/admin")
+public class AdminController {
 
     @GetMapping()
-    public String index() {
-        return "index";
+    public  String admin() {
+        return "admin";
     }
-
-
-
 }
---
+++ src/main/java/ru/kata/spring/boot_security/demo/model/User.java
@@ -0,0 +1,71 @@
+package ru.kata.spring.boot_security.demo.model;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "players")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String nickName;
+    private String playerClass;
+    private int playerLevel;
+
+    public User() {
+
+    }
+
+    public User(String nickName, String playerClass, int playerLevel) {
+        this.nickName = nickName;
+        this.playerClass = playerClass;
+        this.playerLevel = playerLevel;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public String getPlayerClass() {
+        return playerClass;
+    }
+
+    public void setPlayerClass(String playerClass) {
+        this.playerClass = playerClass;
+    }
+
+    public int getPlayerLevel() {
+        return playerLevel;
+    }
+
+    public void setPlayerLevel(int playerLevel) {
+        this.playerLevel = playerLevel;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id && playerLevel == user.playerLevel && Objects.equals(nickName, user.nickName) && Objects.equals(playerClass, user.playerClass);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, nickName, playerClass, playerLevel);
+    }
+}
