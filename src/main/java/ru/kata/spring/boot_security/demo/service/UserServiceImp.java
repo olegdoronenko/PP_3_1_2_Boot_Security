b21a9127af7558e58026e0ff8fc7ce21e17ee6f3
olegdoronenko <61374593+olegdoronenko@users.noreply.github.com>
2023-06-17 08:54:52 +0300

    ауф - инМемори перенесены частично классы из старого проекта

---
+++ src/main/java/ru/kata/spring/boot_security/demo/service/UserServiceImp.java
@@ -0,0 +1,52 @@
+package ru.kata.spring.boot_security.demo.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.kata.spring.boot_security.demo.dao.UserDao;
+import ru.kata.spring.boot_security.demo.model.User;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Service
+@Transactional
+public class UserServiceImp implements UserService{
+
+    private UserDao userDao;
+
+    @Autowired
+    public UserServiceImp(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+
+    @Override
+    public void addUser(User user) {
+        userDao.addUser(user);
+    }
+
+    @Override
+    public void deleteUser(User user) {
+        userDao.deleteUser(user);
+    }
+
+    @Override
+    public void deleteUser(long id) {
+        userDao.deleteUser(id);
+    }
+
+    @Override
+    public void modifyUser(User user) {
+        userDao.modifyUser(user);
+    }
+
+    @Override
+    public User findUserById(long id) {
+        return userDao.findUserById(id);
+    }
+
+    @Override
+    public List<User> getUsersList() {
+        return userDao.getUsersList();
+    }
+}
